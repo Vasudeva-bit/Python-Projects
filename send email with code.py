0a49e76cc635a8baf45d4349aa4b8269758ea107
Vasudeva Kilaru <70791259+Vasudeva-bit@users.noreply.github.com>
2022-04-15 04:29:34 +0530

    Update send email with code.py

--- send email with code.py
+++ send email with code.py
@@ -5,7 +5,7 @@ from pathlib import Path
 html=Template(Path('ht.html').read_text())
 email=EmailMessage()
 email['from']='Vasudeva'
-email['to']='kilaru.kilaru@gmail.com'
+email['to']='k.k@gmail.com'
 email['subject']='Hi'
 #email.set_content('greetings:hiiiii!')
 email.set_content(html.substitute({'name':'dingDing'}),'html')
