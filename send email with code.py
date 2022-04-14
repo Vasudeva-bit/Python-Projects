af6ca67ced657d97dc80a030b384cc788a1f5b9f
Vasudeva Kilaru <70791259+Vasudeva-bit@users.noreply.github.com>
2022-04-15 04:29:56 +0530

    Update send email with code.py

--- send email with code.py
+++ send email with code.py
@@ -10,8 +10,8 @@ email['subject']='Hi'
 #email.set_content('greetings:hiiiii!')
 email.set_content(html.substitute({'name':'dingDing'}),'html')
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp :
-smtp.ehlo()
-smtp.starttls()
-smtp.login('some.address@email.com','xxxxx@yyyy')
-smtp.send_message(email)
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('some.address@email.com','xxxxx@yyyy')
+  smtp.send_message(email)
 print('ran well')
